622d1808148158587b0a9f6aff3762d3ba0b43d1
VladiVK <kozobon@gmail.com>
2020-02-13 11:18:37 +0200

    Initial commit

---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      Main content
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+ return (
+     <div className="header">
+        <img 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxzhQTD5lh4K4rrKoIPfEECknug80XROLM-JUgac3EkTa3SwVH"
+          alt="logotype"/>
+     </div>
+
+ );
+}
+export default Header
