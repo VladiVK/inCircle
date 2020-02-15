fd676995d700412ff571cc4ddb86fd059ed293c6
VladiVK <kozobon@gmail.com>
2020-02-15 18:59:30 +0200

    some test change

--- src/components/Profile/Myposts/Post/Post.js
+++ src/components/Profile/Myposts/Post/Post.js
@@ -1,18 +1,18 @@
 import React from "react";
-import s from './Post.module.css';
+import s from "./Post.module.css";
 
-const Post = (props) => {
+const Post = props => {
   return (
-        <div className={s.item}>
-          <img 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFB2qaIE08h1iDcEZLSHgrngdLW2kFkGLPLcVDO6_LVN5d3XSr"
-            alt=""
-          />
-          <span>User: {props.message}</span>
-          <div>
-            <span>likes: {props.likesCounter}</span>
-          </div>
-        </div>
+    <div className={s.item}>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFB2qaIE08h1iDcEZLSHgrngdLW2kFkGLPLcVDO6_LVN5d3XSr"
+        alt=""
+      />
+      <span>User: {props.message}</span>
+      <div>
+        <span>likes: {props.likesCounter}</span>
+      </div>
+    </div>
   );
 };
 
