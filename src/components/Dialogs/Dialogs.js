76e03687ffdf68c13ed0a79e665397446c82cc85
VladiVK <kozobon@gmail.com>
2020-02-16 17:49:12 +0200

    lesson 21 create Dialog page Grid layout

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return (
-        <div className={s.content}>
-            <h1>Dialogs</h1>
-        </div>
-    );
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Homer</div>
+        <div className={s.dialog}>Bob</div>
+        <div className={s.dialog}>Janpol</div>
+        <div className={`${s.dialog} ${s.active}`}>Louis-Ferdinand</div>
+        <div className={s.dialog}>Romain Gary</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>Morning glory</div>
+        <div className={s.message}>Evening happy</div>
+      </div>
+    </div>
+  );
+};
 export default Dialogs;
