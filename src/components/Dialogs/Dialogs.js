1ef3d3c31a7185b25e3b0e494a5b16da34d69103
VladiVK <kozobon@gmail.com>
2020-02-17 11:15:03 +0200

    lesson 24 /preparing for method [map] /

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -18,21 +18,34 @@ const Message = props => {
 };
 
 const Dialogs = (props) => {
+
+  const dialosData = [
+    {id: 1, name: 'Homer'},
+    {id: 2, name: 'Bob'},
+    {id: 3, name: 'Janpol'},
+    {id: 4, name: 'Louis-Ferdinand'},
+    {id: 5, name: 'Romain Gary'},
+  ];
+  const messagesData = [
+    {id: 1, message: 'Salut, comment vas-tu'},
+    {id: 2, message: 'Hello, how are you?'},
+    {id: 3, message: 'Ciao, come stai'},
+    {id: 4, message: 'Hallo, wie geht es dir'},
+    {id: 5, message: 'Hola, como estas'},
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Homer" id="1" />
-        <DialogItem name="Bob" id="2" />
-        <DialogItem name="Janpol" id="3" />
-        <DialogItem name="Louis-Ferdinand" id="4" />
-        <DialogItem name="Romain Gary" id="5" />
+        <DialogItem name={dialosData[0].name}  />
+        <DialogItem name={dialosData[1].name}  />
       </div>
       {/* <div className={`${s.dialog} ${s.active}`}> */}
 
       <div className={s.messages}>
-        <Message message='Bonjour' />
-        <Message message='Hello' />
-        <Message message='Ciao' />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        
       </div>
     </div>
   );
--- src/components/Profile/Myposts/Myposts.js
+++ src/components/Profile/Myposts/Myposts.js
@@ -3,6 +3,13 @@ import s from './Myposts.module.css';
 import Post from "./Post/Post.js";
 
 const Myposts = () => {
+  const postData = [
+    {id: 1, message: 'Hello, glad to see you again!', likesCounter: 12},
+    {id: 2, message: 'Good morning', likesCounter: 16},
+    {id: 3, message: 'Have a nice day!', likesCounter: 10},
+    {id: 4, message: 'How are you?', likesCounter: 11},
+    {id: 5, message: 'Where are you?', likesCounter: 0},
+  ];
   return (
  
       <div className={s.postsBlock}>
@@ -13,26 +20,10 @@ const Myposts = () => {
         <div>
           <button>Add Post</button>
         </div>
-        <Post
-          message={'Hello, glad to see you again!'}
-          likesCounter={10}
-        />
-        <Post 
-          message={'Hi, bro! I am too!'}
-          likesCounter={15}
-        /> 
-        <Post 
-          message={'Hi, bro! I am too!'}
-          likesCounter={15}
-        /> 
-        <Post 
-          message={'Hi, bro! I am too!'}
-          likesCounter={15}
-        /> 
-        <Post 
-          message={'Hi, bro! I am too!'}
-          likesCounter={15}
-        /> 
+        <Post message={postData[0].message} likesCounter={postData[0].likesCounter} />
+        <Post message={postData[1].message} likesCounter={postData[1].likesCounter} />
+        <Post message={postData[2].message} likesCounter={postData[2].likesCounter} />
+      
       </div>
     
   );
